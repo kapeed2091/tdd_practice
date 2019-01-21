fabf678034f0446f459fbaf6b2db5c4cb20d96b1
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-01-21 19:31:30 +0530

    added test_case of transfer_balance

---
+++ wallet/tests/test_transfer_balance.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+from wallet.models import Account
+
+
+class TestTransferBalance(TestCase):
+    sender_customer_id = 'sender'
+    receiver_customer_id = 'receiver'
+
+    def setUp(self):
+        sender_account_details = Account.create_account(self.sender_customer_id)
+        receiver_account_details = Account.create_account(
+            self.receiver_customer_id)
+
+        Account.add_balance(self.sender_customer_id, 50)
+        Account.add_balance(self.receiver_customer_id, 30)
+
+    def testcase_transfer_balance(self):
+        sender_old_balance = Account.get_balance(self.sender_customer_id)
+        receiver_old_balance = Account.get_balance(self.receiver_customer_id)
+
+        Account.transfer_balance(
+            self.sender_customer_id, self.receiver_customer_id, 10)
+
+        sender_new_balance = Account.get_balance(self.sender_customer_id)
+        receiver_new_balance = Account.get_balance(self.receiver_customer_id)
+
+        self.assertEquals(sender_old_balance, sender_new_balance+10)
+        self.assertEquals(receiver_old_balance, receiver_new_balance-10)
