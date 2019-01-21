28378fa99eef3bdf5c1cec30b742cad49851cad7
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-01-21 17:24:11 +0530

    created tests & code for get_balance

--- wallet/tests/test_get_balance.py
+++ wallet/tests/test_get_balance.py
@@ -4,11 +4,15 @@ from django.test import TestCase
 class TestGetBalance(TestCase):
     customer_id = 'customer1'
 
-
     def testcase_get_new_user_account_balance(self):
         from wallet.models import Account
         Account.create_account(self.customer_id)
         balance = Account.get_balance(self.customer_id)
         self.assertEquals(balance, 0,
                           'Incorrect balance for new account')
+
+    def testcase_get_balance_without_customer_account(self):
+        from wallet.models import Account
+        self.assertRaises(Account.DoesNotExist,
+                          lambda : Account.get_balance(self.customer_id))
 
