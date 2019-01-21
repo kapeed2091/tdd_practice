6eb0256d3e697602529f7314b598164e5fb4de40
vedavidh <vedavidhbudimuri@gmail.com>
2019-01-21 19:31:24 +0530

    Refactoring step: 1) Removed data dependency between test and model (empty list) 2) Added account field to transaction as we need to query on customer_id 3) Added amount field to transaction as it is obvious

--- wallet/models/transaction.py
+++ wallet/models/transaction.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Transaction(models.Model):
+    account = models.ForeignKey('wallet.Account')
+    amount = models.IntegerField()
 
     @classmethod
     def get_statement(cls, customer_id):
-        return []
+        return cls.objects.filter(account__customer_id=customer_id)
