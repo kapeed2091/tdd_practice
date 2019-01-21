940e0c15432c3cfb0e740e5cd27604d69b029922
vedavidh <vedavidhbudimuri@gmail.com>
2019-01-21 19:42:57 +0530

    Added test case for usecase in Get Statement: 1) Added test case, luckily it is passing as the obvious implementation was sufficient for this case as well

--- wallet/models/transaction.py
+++ wallet/models/transaction.py
@@ -6,5 +6,11 @@ class Transaction(models.Model):
     amount = models.IntegerField()
 
     @classmethod
+    def create_transaction_for_account_id(cls, account_id, amount):
+        obj = cls(account_id=account_id, amount=amount)
+        obj.save()
+        return obj
+
+    @classmethod
     def get_statement(cls, customer_id):
         return cls.objects.filter(account__customer_id=customer_id)
--- wallet/tests/test_get_statement.py
+++ wallet/tests/test_get_statement.py
@@ -4,8 +4,42 @@ from django.test import TestCase
 class TestGetStatement(TestCase):
     customer_id = 'customer1'
 
+    TRANSACTIONS_DICT = [
+        {
+            'account_id': 1,
+            'amount': 1
+        },
+        {
+            'account_id': 1,
+            'amount': 2
+        }
+    ]
+
+    def create_account(self):
+        from wallet.models import Account
+        Account.create_account(customer_id=self.customer_id)
+
+    def create_transactions(self):
+        import copy
+        from wallet.models import Transaction
+
+        for item in copy.deepcopy(self.TRANSACTIONS_DICT):
+            Transaction.create_transaction_for_account_id(
+                account_id=item['account_id'],
+                amount=item['amount']
+            )
+
     def test_get_customer_statement_for_user_without_transactions(self):
         from wallet.models import Transaction
 
         transactions = Transaction.get_statement(customer_id=self.customer_id)
         self.assertEqual(len(transactions), 0)
+
+    def test_get_statement_for_user_with_transactions(self):
+        from wallet.models import Transaction
+
+        self.create_account()
+        self.create_transactions()
+
+        transactions = Transaction.get_statement(customer_id=self.customer_id)
+        self.assertEqual(len(transactions), 2)
