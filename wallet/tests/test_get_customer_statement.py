54c123f9282f9435b06985b6a4ab1a2c48967c1c
Lokesh Dokara <lokesh@ibhubs.co>
2019-01-21 20:23:15 +0530

    Refactored Testcases

--- wallet/tests/test_get_customer_statement.py
+++ wallet/tests/test_get_customer_statement.py
@@ -8,10 +8,16 @@ class TestGetCustomerStatement(test.TestCase):
         account_details = Account.create_account(self.customer_id)
         self.account_id = account_details['account_id']
         Account.add_balance(self.customer_id, 10)
+        from wallet import models
+        self.transactions = models.Transaction.get_customer_statement(
+            self.customer_id
+        )
+    def test_check_transaction_count(self):
+        self.assertEqual(len(self.transactions), 1)
 
     def test_get_user_statement(self):
-        from wallet import models, entities
-        transactions = models.Transaction.get_customer_statement(self.customer_id)
-        self.assertEqual(len(transactions), 1)
-        transaction_entity = entities.Transaction.create_credit_transaction(amount=10)
-        self.assertEqual(transactions[0], transaction_entity)
+        from wallet import entities
+        transaction_entity = entities.Transaction.create_credit_transaction(
+            amount=10
+        )
+        self.assertEqual(self.transactions[0], transaction_entity)
