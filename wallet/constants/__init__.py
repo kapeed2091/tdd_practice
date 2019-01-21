70e9d4f5a2294d308263ca668d12269b84b46711
Lokesh Dokara <lokesh@ibhubs.co>
2019-01-21 20:35:06 +0530

    Red state transfer type

---
+++ wallet/constants/__init__.py
@@ -0,0 +1,5 @@
+import enum
+
+class TransferType(enum.Enum):
+    EXTERNAL = 'EXTERNAL'
+    INTERNAL = 'INTERNAL'
--- wallet/entities/transaction.py
+++ wallet/entities/transaction.py
@@ -1,6 +1,7 @@
 class Transaction(object):
-    def __init__(self, transaction_type, amount):
+    def __init__(self, transaction_type, transfer_type, amount):
         self.transaction_type = transaction_type
+        self.transfer_type = transfer_type
         self.amount = amount
 
     @classmethod
@@ -8,7 +9,8 @@ class Transaction(object):
         return cls(None, amount)
 
     def __eq__(self, other):
-        return (self.transaction_type == other.transaction_type and
-                self.amount == other.amount)
-
-
+        return (
+            self.transaction_type == other.transaction_type and
+            self.transfer_type == other.transfer_type and
+            self.amount == other.amount
+        )
