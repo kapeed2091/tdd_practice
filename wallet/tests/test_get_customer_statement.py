fbfcedf2e3a6c6a598f81ef315b3bc353bc92843
Lokesh Dokara <lokesh@ibhubs.co>
2019-01-21 20:01:13 +0530

    Verifying transaction contents error testcase

--- wallet/tests/test_get_customer_statement.py
+++ wallet/tests/test_get_customer_statement.py
@@ -10,6 +10,8 @@ class TestGetCustomerStatement(test.TestCase):
         Account.add_balance(self.customer_id, 10)
 
     def test_get_user_statement(self):
-        from wallet import models
+        from wallet import models, entities
         transactions = models.Transaction.get_customer_statement(self.customer_id)
         self.assertEqual(len(transactions), 1)
+        transaction_entity = entities.Transaction.create_credit_transaction(amount=10)
+        self.assertEqual(transactions[0], transaction_entity)
