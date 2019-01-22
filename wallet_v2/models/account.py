8a7be2cb9b94775699f8637e03dcab0a0215abcd
navya <navya.d@ibhubs.co>
2019-01-22 20:32:07 +0530

    removed account_id from account model

--- wallet_v2/models/account.py
+++ wallet_v2/models/account.py
@@ -3,19 +3,9 @@ from django.db import models
 
 class Account(models.Model):
     CUSTOMER_ID_LENGTH = 20
-    ACCOUNT_ID_LENGTH = 20
     customer_id = models.CharField(max_length=CUSTOMER_ID_LENGTH)
-    account_id = models.CharField(max_length=ACCOUNT_ID_LENGTH)
 
     @classmethod
     def create_account(cls, customer_id):
-        account_id = cls.generate_account_id(cls.ACCOUNT_ID_LENGTH)
-        account = cls.objects.create(customer_id=customer_id,
-                                     account_id=account_id)
-        return {'customer_id': account.customer_id,
-                'account_id': account.account_id}
-
-    @staticmethod
-    def generate_account_id(length):
-        import uuid
-        return str(uuid.uuid4())[: length]
+        account = cls.objects.create(customer_id=customer_id)
+        return {'customer_id': account.customer_id}
