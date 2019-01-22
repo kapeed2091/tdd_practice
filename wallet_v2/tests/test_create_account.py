49073c08833d6ba9757ed0d9a18b5f67ad923bd8
navya <navya.d@ibhubs.co>
2019-01-22 20:35:30 +0530

    added test_case_2 for create_account -->  compilation error

--- wallet_v2/tests/test_create_account.py
+++ wallet_v2/tests/test_create_account.py
@@ -8,3 +8,12 @@ class TestCreateAccount(TestCase):
         from wallet_v2.models import Account
         account_details = Account.create_account(customer_id)
         self.assertEqual(account_details['customer_id'], customer_id)
+
+    def testcase_different_account_for_different_users(self):
+        customer_id_1 = 'customer1'
+        customer_id_2 = 'customer2'
+        from wallet_v2.models import Account
+        account_id_1 = Account.create_account(customer_id_1)['account_id']
+        account_id_2 = Account.create_account(customer_id_2)['account_id']
+
+        self.assertNotEqual(account_id_1, account_id_2, 'AccountIds Matched.!')
