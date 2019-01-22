63a283b20145cc78b9a0fb4cbb6cd2fb86bf6ee0
navya <navya.d@ibhubs.co>
2019-01-22 07:25:44 +0530

    implemented test_case_2 for create account

--- wallet/models/account.py
+++ wallet/models/account.py
@@ -12,4 +12,18 @@ class Account(models.Model):
 
     @classmethod
     def create_account(cls, customer_id):
-        return {'account_id': '', 'customer_id': ''}
+        try:
+            cls.objects.get(customer_id=customer_id)
+            return Exception
+        except cls.DoesNotExist:
+
+            account_id = cls.generate_account_id(cls.ACCOUNT_ID_LENGTH)
+            account = cls.objects.create(
+                customer_id=customer_id, account_id=account_id)
+        return {'account_id': account.account_id,
+                'customer_id': account.customer_id}
+
+    @staticmethod
+    def generate_account_id(length):
+        import uuid
+        return str(uuid.uuid4())[0:length]
--- wallet/tests/test_create_account.py
+++ wallet/tests/test_create_account.py
@@ -3,10 +3,22 @@ from django.test import TestCase
 
 class TestCreateAccount(TestCase):
 
-    def test_create_account(self):
+    def testcase_create_account(self):
         from wallet.models import Account
         customer_id = 'customer1'
         account_details = Account.create_account(customer_id)
 
         self.assertEqual(account_details['customer_id'], customer_id,
                          "customer_id mismatch")
+
+    def testcase_different_accounts_for_different_users(self):
+        from wallet.models import Account
+        customer_1_id = 'customer1'
+        customer_2_id = 'customer2'
+
+        customer_1_details = Account.create_account(customer_1_id)
+        customer_2_details = Account.create_account(customer_2_id)
+
+        self.assertNotEqual(
+            customer_1_details['account_id'], customer_2_details['account_id'],
+            "same account numbers for different accounts")
