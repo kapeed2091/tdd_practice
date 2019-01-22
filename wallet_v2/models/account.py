ebad0b4b8f24df2fb9bf21c555cd9c79c79847a5
navya <navya.d@ibhubs.co>
2019-01-22 20:40:12 +0530

    test_case_2 for create_account -->  green state

--- wallet_v2/models/account.py
+++ wallet_v2/models/account.py
@@ -3,9 +3,15 @@ from django.db import models
 
 class Account(models.Model):
     CUSTOMER_ID_LENGTH = 20
+    ACCOUNT_ID_LENGTH = 20
     customer_id = models.CharField(max_length=CUSTOMER_ID_LENGTH)
+    account_id = models.CharField(max_length=ACCOUNT_ID_LENGTH)
 
     @classmethod
     def create_account(cls, customer_id):
-        account = cls.objects.create(customer_id=customer_id)
-        return {'customer_id': account.customer_id, 'account_id': ''}
+        import uuid
+        account_id = str(uuid.uuid4())[: cls.ACCOUNT_ID_LENGTH]
+        account = cls.objects.create(customer_id=customer_id,
+                                     account_id=account_id)
+        return {'customer_id': account.customer_id,
+                'account_id': account.account_id}
