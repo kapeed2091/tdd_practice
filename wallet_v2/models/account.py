69dd1a922702aa4eeff281bf2d7b17fe75768b99
navya <navya.d@ibhubs.co>
2019-01-22 20:36:46 +0530

    test_case_2 for create_account -->  red state

--- wallet_v2/models/account.py
+++ wallet_v2/models/account.py
@@ -8,4 +8,4 @@ class Account(models.Model):
     @classmethod
     def create_account(cls, customer_id):
         account = cls.objects.create(customer_id=customer_id)
-        return {'customer_id': account.customer_id}
+        return {'customer_id': account.customer_id, 'account_id': ''}
