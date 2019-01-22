b4b3ad15cb4a60064e6bb31c40a819905005dead
navya <navya.d@ibhubs.co>
2019-01-22 21:10:08 +0530

    test_case_1 for add_balance - compilation error

---
+++ wallet_v2/tests/test_add_balance.py
@@ -0,0 +1,13 @@
+from django.test import TestCase
+
+
+class TestAddBalance(TestCase):
+    customer_id = 'customer1'
+
+    def testcase_add_balance_to_user(self):
+        from wallet_v2.models import Account
+        Account.create_account(self.customer_id)
+        initial_balance = Account.get_balance(self.customer_id)
+        Account.add_balance(self.customer_id, 1000)
+        balance = Account.get_balance(self.customer_id)
+        self.assertEqual(balance - initial_balance, 1000)
