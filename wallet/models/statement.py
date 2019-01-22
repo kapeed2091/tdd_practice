b9b1a85a88da7706f06bcf23cb1c9aabe432d946
Ratan Rithwik <rithwik@shirley.local>
2019-01-22 21:31:04 +0530

    Green State: get_statement successful test case

--- wallet/models/statement.py
+++ wallet/models/statement.py
@@ -2,12 +2,29 @@ from django.db import models
 
 
 class Statement(models.Model):
-    pass
-
-    @classmethod
-    def add_transactions(cls, transactions):
-        pass
+    date_time = models.DateTimeField()
+    amount = models.PositiveIntegerField()
+    status = models.CharField(max_length=20)
+    customer_id = models.CharField(max_length=20)
 
     @classmethod
     def get_transactions(cls, date_range):
-        pass
+        from_date = date_range["from_date"]
+        to_date = date_range["to_date"]
+
+        query_set = cls.objects.filter(date_time__range=(from_date, to_date))
+
+        transactions = []
+
+        for each in query_set:
+            transactions.append(each.convert_to_dict())
+
+        return transactions
+
+    def convert_to_dict(self):
+        return {
+            "date_time": self.date_time,
+            "amount": self.amount,
+            "status": self.status,
+            "customer_id": self.customer_id
+        }
--- wallet/tests/test_get_statement.py
+++ wallet/tests/test_get_statement.py
@@ -1,47 +1,70 @@
 from django.test import TestCase
-import datetime
 
 
 class TestGetStatement(TestCase):
     customer_id = 'customer1'
     transactions = [
         {
-            "date_time": datetime.datetime(2017, 12, 12, 13, 0, 0),
+            "date_time": "2017-12-12 13:00:00",
             "amount": 100,
-            "status": "DEBIT"
+            "status": "DEBIT",
+            "customer_id": 'customer1'
         },
         {
-            "date_time": datetime.datetime(2017, 12, 13, 13, 0, 0),
+            "date_time": "2017-12-13 13:00:00",
             "amount": 110,
-            "status": "CREDIT"
+            "status": "CREDIT",
+            "customer_id": 'customer1'
         },
         {
-            "date_time": datetime.datetime(2017, 12, 14, 13, 0, 0),
+            "date_time": "2017-12-14 13:00:00",
             "amount": 120,
-            "status": "DEBIT"
+            "status": "DEBIT",
+            "customer_id": 'customer1'
         },
         {
-            "date_time": datetime.datetime(2017, 12, 15, 13, 0, 0),
+            "date_time": "2017-12-15 13:00:00",
             "amount": 130,
-            "status": "CREDIT"
+            "status": "CREDIT",
+            "customer_id": 'customer1'
         }
     ]
 
     def setUp(self):
         from wallet.models import Statement
-        Statement.add_transactions(self.transactions)
+        for each in self.transactions:
+            Statement.objects.create(
+                date_time=each.get("date_time"),
+                amount=each.get("amount"),
+                status=each.get("status"),
+                customer_id=each["customer_id"]
+            )
 
     def test_get_balance_successful(self):
         from wallet.models import Statement
 
-        from_date = datetime.datetime(2017, 12, 12, 12, 0, 0)
-        to_date = datetime.datetime(2017, 12, 16, 12, 0, 0)
+        from_date = "2017-12-12 13:00:00"
+        to_date = "2017-12-16 13:00:00"
+
+        from ib_common.date_time_utils.convert_string_to_local_date_time \
+            import convert_string_to_local_date_time
+        date_time_format = '%Y-%m-%d %H:%M:%S'
 
         date_range = {
-            "from_date": from_date,
-            "to_date": to_date
+            "from_date": convert_string_to_local_date_time(from_date,
+                                                           date_time_format),
+            "to_date": convert_string_to_local_date_time(to_date,
+                                                         date_time_format)
         }
 
         transactions = Statement.get_transactions(date_range=date_range)
 
-        self.assertItemsEqual(self.transactions, transactions)
+        transactions_input = []
+        import copy
+        for each in self.transactions:
+            dict_ = copy.deepcopy(each)
+            dict_["date_time"] = convert_string_to_local_date_time(
+                each["date_time"], date_time_format)
+            transactions_input.append(dict_)
+
+        self.assertItemsEqual(transactions_input, transactions)
