80297e77db953fa6ab11ad5e0d5f8b17c74a677a
revanth <revanth.1518@gmail.com>
2019-01-23 10:58:38 +0530

    added testcase test_get_statement_with_one_transaction compilation error

--- wallet/tests/test_get_statement.py
+++ wallet/tests/test_get_statement.py
@@ -14,3 +14,20 @@ class TestGetStatement(TestCase):
         statement = Transaction.get_statement(self.customer_id)
         self.assertEquals(statement['total'], 0)
         self.assertEquals(statement['transactions'], [])
+
+    def test_get_statement_with_one_transaction(self):
+        from wallet.models import Transaction
+        from datetime import datetime
+        Transaction.objects.create(
+            transaction_id='abcd', date=datetime.today(), amount=100,
+            balance=100, transaction_type='CREDIT')
+        statement = Transaction.get_statement(self.customer_id)
+        self.assertEquals(statement['total'], 1)
+        self.assertEquals(statement['transactions'], [{
+            'transaction_id': 'abcd',
+            'date': datetime.now().strftime("%Y-%m-%d"),
+            'amount': 100,
+            'balance': 100,
+            'transaction_type': 'CREDIT'
+        }])
+
