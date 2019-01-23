eaf28c298e52050d99eeb0b258030e66166470aa
Ratan Rithwik <rithwik@shirley.local>
2019-01-23 09:58:16 +0530

    Refactoring: invalid date range test case

--- wallet/models/statement.py
+++ wallet/models/statement.py
@@ -9,6 +9,21 @@ class Statement(models.Model):
 
     @classmethod
     def get_transactions(cls, customer_id, date_range):
+        cls._date_range_validation(date_range=date_range)
+
+        date_range_tuple = cls._get_date_range_tuple(date_range=date_range)
+        query_set = cls.objects.filter(
+            customer_id=customer_id, date_time__range=date_range_tuple
+        )
+
+        transactions = []
+        for each in query_set:
+            transactions.append(each.convert_to_dict())
+
+        return transactions
+
+    @staticmethod
+    def _date_range_validation(date_range):
         from_date = date_range["from_date"]
         to_date = date_range["to_date"]
 
@@ -17,16 +32,9 @@ class Statement(models.Model):
             from wallet.constants.exception_constants import INVALID_DATE_RANGE
             raise InvalidDateRangeException(INVALID_DATE_RANGE)
 
-        query_set = cls.objects.filter(
-            customer_id=customer_id, date_time__range=(from_date, to_date)
-        )
-
-        transactions = []
-
-        for each in query_set:
-            transactions.append(each.convert_to_dict())
-
-        return transactions
+    @staticmethod
+    def _get_date_range_tuple(date_range):
+        return date_range["from_date"], date_range["to_date"]
 
     def convert_to_dict(self):
         return {
