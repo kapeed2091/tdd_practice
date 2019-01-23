bea0bf09169716a70f48e55192e8e22681636a0a
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-01-23 20:22:37 +0530

    N1 - Choose Descriptive names

--- wallet/models/transaction.py
+++ wallet/models/transaction.py
@@ -27,11 +27,11 @@ class Transaction(models.Model):
         return transactions_list
 
     @classmethod
-    def add_customer_transaction(cls, customer_id, transaction_amount, type):
+    def add_customer_transaction(cls, customer_id, transaction_amount, transaction_type):
         transaction_id = cls.generate_transaction_id(cls.TRANSACTION_ID_LENGTH)
         cls.assign_transaction_id(customer_id=customer_id,
                                   transaction_id=transaction_id, amount=transaction_amount,
-                                  type=type)
+                                  type=transaction_type)
 
     @classmethod
     def assign_transaction_id(cls, customer_id, transaction_id, amount, type):
