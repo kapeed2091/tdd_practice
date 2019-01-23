d81c05a7346789d1d128f3ba050c5a6f0cacc710
navya <navya.d@ibhubs.co>
2019-01-23 13:15:14 +0530

    test_case_2 for transfer_balance function - red state

--- wallet_v2/tests/test_transfer_balance.py
+++ wallet_v2/tests/test_transfer_balance.py
@@ -26,3 +26,14 @@ class TestTransferBalance(TestCase):
         self.assertEqual(
             post_transfer_beneficiary_balance - pre_transfer_beneficiary_balance,
             100)
+
+    def testcase_transfer_negative_balance(self):
+        from wallet_v2.models import Account
+
+        Account.create_account(self.payee_customer_id)
+        Account.create_account(self.beneficiary_customer_id)
+
+        with self.assertRaisesMessage(
+                Exception, "Transfer amount should be greater than zero"):
+            Account.transfer_balance(self.payee_customer_id,
+                                     self.beneficiary_customer_id, -2)
