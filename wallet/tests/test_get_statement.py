ee2411f2c269d67ed434287ec691a2c6cacf29ff
revanth <revanth.1518@gmail.com>
2019-01-23 09:56:42 +0530

    modified testcase

--- wallet/tests/test_get_statement.py
+++ wallet/tests/test_get_statement.py
@@ -10,7 +10,7 @@ class TestGetStatement(TestCase):
     customer_id = 'Customer1'
 
     def test_get_statement(self):
-        from wallet.models import Statement
-        statement = Statement.get_statement(self.customer_id)
+        from wallet.models import Transaction
+        statement = Transaction.get_statement(self.customer_id)
         self.assertEquals(statement['total'], 0)
         self.assertEquals(statement['transactions'], [])
