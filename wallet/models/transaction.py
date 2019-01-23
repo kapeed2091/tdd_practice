0bc33438adc9d1d691fe8321f2bdef5baa3d6518
revanth <revanth.1518@gmail.com>
2019-01-23 10:03:07 +0530

    testcase test_get_statement in red state, added migration for Transaction model

---
+++ wallet/models/transaction.py
@@ -0,0 +1,16 @@
+"""
+Created on 2019-01-23
+
+@author: revanth
+"""
+from django.db import models
+
+
+class Transaction(models.Model):
+
+    @classmethod
+    def get_statement(cls, customer_id):
+        return {
+            'total': -1,
+            'transactions': []
+        }
