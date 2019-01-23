99271dad7b18bb745f7c4b8b1264364b5ead52e4
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-23 09:20:34 +0530

    TestCase: Get Transactions, state: Green

--- wallet/models/transaction.py
+++ wallet/models/transaction.py
@@ -4,3 +4,22 @@ from django.db import models
 class Transaction(models.Model):
 
     account = models.ForeignKey('account')
+    amount = models.IntegerField(default=0)
+
+    @classmethod
+    def create_obj(cls, account, amount):
+        return cls.objects.create(
+            account=account,
+            amount=amount
+        )
+
+    @classmethod
+    def get_transactions(cls, customer_id):
+        transactions = cls.objects.filter(account__customer_id=customer_id)
+        return [each.convert_to_dict() for each in transactions]
+
+    def convert_to_dict(self):
+        return {
+            'customer_id': self.account.customer_id,
+            'amount': self.amount
+        }
