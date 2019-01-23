9cb6155c150c418341c27163433837e0f1094b7e
Ratan Rithwik <rithwik@shirley.local>
2019-01-23 09:33:07 +0530

    Green state: test case with no transactions

--- wallet/tests/test_get_statement.py
+++ wallet/tests/test_get_statement.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 
 class TestGetStatement(TestCase):
     customer_id = 'customer1'
+    no_transactions_customer_id = 'customer3'
     transactions = [
         {
             "date_time": "2017-12-12 13:00:00",
@@ -70,4 +71,39 @@ class TestGetStatement(TestCase):
 
         customer_transactions = [each for each in self.transactions if
                                  each["customer_id"] == self.customer_id]
+        self.assertItemsEqual(customer_transactions, transactions)
+
+    def test_case_no_transactions_for_customer(self):
+        self.setup_statements_for_both_customers()
+
+        from wallet.models import Statement
+
+        from_date = "2017-12-12 13:00:00"
+        to_date = "2017-12-16 13:00:00"
+
+        from ib_common.date_time_utils.convert_string_to_local_date_time \
+            import convert_string_to_local_date_time
+        from ib_common.date_time_utils.convert_datetime_to_local_string \
+            import convert_datetime_to_local_string
+        date_time_format = '%Y-%m-%d %H:%M:%S'
+
+        date_range = {
+            "from_date": convert_string_to_local_date_time(from_date,
+                                                           date_time_format),
+            "to_date": convert_string_to_local_date_time(to_date,
+                                                         date_time_format)
+        }
+
+        transactions = Statement.get_transactions(
+            customer_id=self.no_transactions_customer_id,
+            date_range=date_range
+        )
+
+        for each in transactions:
+            each["date_time"] = convert_datetime_to_local_string(
+                each["date_time"], date_time_format)
+
+        customer_transactions = \
+            [each for each in self.transactions if
+             each["customer_id"] == self.no_transactions_customer_id]
         self.assertItemsEqual(customer_transactions, transactions)
