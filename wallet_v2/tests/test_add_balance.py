7c57e3f38b0b14a6dd3e08b9b13b7a303b5d88e8
navya <navya.d@ibhubs.co>
2019-01-23 08:08:09 +0530

    test_case_2 for add_balance - red state

--- wallet_v2/tests/test_add_balance.py
+++ wallet_v2/tests/test_add_balance.py
@@ -11,3 +11,9 @@ class TestAddBalance(TestCase):
         Account.add_balance(self.customer_id, 1000)
         balance = Account.get_balance(self.customer_id)
         self.assertEqual(balance - initial_balance, 1000)
+
+    def testcase_add_negative_balance_to_user(self):
+        from wallet_v2.models import Account
+        Account.create_account(customer_id=self.customer_id)
+        with self.assertRaises(Exception):
+            Account.add_balance(self.customer_id, -10)
