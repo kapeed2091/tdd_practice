6340d76597e0cf17e2ba1c3fdf375d61243f1eed
navya <navya.d@ibhubs.co>
2019-01-23 19:41:04 +0530

    test_case_4 for get_statement - refactored

--- wallet_v2/models/transaction.py
+++ wallet_v2/models/transaction.py
@@ -25,27 +25,39 @@ class Transaction(models.Model):
     @classmethod
     def get_customer_statement_within_date_range(
             cls, customer_id, from_date_str, to_date_str):
+        from wallet_v2.models import Account
+
+        from_date, to_date = cls.get_from_and_to_datetime_objects(
+            from_date_str, to_date_str)
+        account = Account.get_account(customer_id)
+        customer_transactions = cls.objects.filter(
+            account_id=account.id,
+            transaction_date__gte=from_date, transaction_date__lte=to_date)
+        return cls.get_transaction_details_with_date(customer_transactions)
+
+    @classmethod
+    def get_from_and_to_datetime_objects(cls, from_date_str, to_date_str):
         from ib_common.date_time_utils.convert_string_to_local_date_time \
             import convert_string_to_local_date_time
-        from ib_common.date_time_utils.convert_datetime_to_local_string import \
-            convert_datetime_to_local_string
         from wallet_v2.constants.general import DEFAULT_DATE_TIME_FORMAT
-        from wallet_v2.models import Account
 
         from_date = convert_string_to_local_date_time(
             from_date_str, DEFAULT_DATE_TIME_FORMAT)
         to_date = convert_string_to_local_date_time(
             to_date_str, DEFAULT_DATE_TIME_FORMAT)
+        return from_date, to_date
 
-        account = Account.get_account(customer_id)
-        customer_transactions = cls.objects.filter(
-            account_id=account.id,
-            transaction_date__gte=from_date, transaction_date__lte=to_date)
+    @classmethod
+    def get_transaction_details_with_date(cls, transactions):
+        from ib_common.date_time_utils.convert_datetime_to_local_string import \
+            convert_datetime_to_local_string
+        from wallet_v2.constants.general import DEFAULT_DATE_TIME_FORMAT
+
         return [
             {
                 'amount': transaction.amount,
                 'transaction_type': transaction.transaction_type,
                 'date': convert_datetime_to_local_string(
                     transaction.transaction_date, DEFAULT_DATE_TIME_FORMAT)
-            } for transaction in customer_transactions
+            } for transaction in transactions
         ]
