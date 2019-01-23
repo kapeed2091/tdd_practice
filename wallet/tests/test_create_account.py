e0f8927d8a1a7b9ba7d0d5c17de58a4f0a372a48
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-23 20:01:24 +0530

    Refactoring: G10-Vertical Seperation

--- wallet/tests/test_create_account.py
+++ wallet/tests/test_create_account.py
@@ -4,8 +4,9 @@ from django.test import TestCase
 class TestCreateAccount(TestCase):
     
     def testcase_create_account(self):
-        customer_id = 'customer1'
         from wallet.models import Account
+
+        customer_id = 'customer1'
         Account.create_account(customer_id)
         account_details = Account.get_account(customer_id)
 
@@ -13,10 +14,10 @@ class TestCreateAccount(TestCase):
         self.assertNotEqual(account_details.account_id, None, 'Issue with account_id')
 
     def testcase_create_multiple_accounts_for_different_users(self):
+        from wallet.models import Account
+
         customer_id_1 = 'customer1'
         customer_id_2 = 'customer2'
-
-        from wallet.models import Account
         Account.create_account(customer_id_1)
         Account.create_account(customer_id_2)
         account_details_1 = Account.get_account(customer_id_1)
@@ -27,21 +28,18 @@ class TestCreateAccount(TestCase):
         self.assertNotEqual(account_details_1.account_id, account_details_2.account_id)
 
     def testcase_multiple_accounts_for_same_user(self):
-        customer_id = 'customer1'
-
         from wallet.models import Account
 
+        customer_id = 'customer1'
         Account.create_account(customer_id)
         self.assertRaises(Exception, lambda: Account.create_account(customer_id))
 
     def testcase_joint_account_holder(self):
-        customer_id_1 = 'customer1'
-        customer_id_2 = 'customer2'
-
         from wallet.models import Account
 
+        customer_id_1 = 'customer1'
+        customer_id_2 = 'customer2'
         account_id = '200802022'
-
         Account._assign_account_id_to_customer(account_id=account_id,
                                                customer_id=customer_id_1)
         self.assertRaises(Exception, lambda: Account._assign_account_id_to_customer(account_id=account_id,
