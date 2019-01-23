4615a15296a3240b120dd50eeb349439d238b5b0
nandeep-tech <nandeep@ibtspl.com>
2019-01-23 10:14:10 +0530

    added transactions validation in testcase_add_balance

--- wallet/tests/test_add_balance.py
+++ wallet/tests/test_add_balance.py
@@ -4,6 +4,8 @@ from django.test import TestCase
 class TestAddBalance(TestCase):
     customer_id = 'customer1'
     account_id = None
+    amount_to_add = 10
+    transaction_type = "CREDIT"
 
     def setUp(self):
         from wallet.models import Account
@@ -14,10 +16,11 @@ class TestAddBalance(TestCase):
         from wallet.models import Account
 
         prev_balance = Account.get_balance(customer_id=self.customer_id)
-        Account.add_balance(self.customer_id, 10)
+        Account.add_balance(self.customer_id, self.amount_to_add)
         balance = Account.get_balance(self.customer_id)
 
-        self.assertEquals(balance, prev_balance+10)
+        self.assertEquals(balance, prev_balance+self.amount_to_add)
+        self._validate_transaction_db_state()
 
     def testcase_add_negative_balance(self):
         from wallet.models import Account
@@ -28,3 +31,14 @@ class TestAddBalance(TestCase):
         from wallet.models import Account
         with self.assertRaises(Exception):
             Account.add_balance(self.customer_id, float(10.5))
+
+    def _validate_transaction_db_state(self):
+        from wallet.models import Transaction
+        transactions = \
+            Transaction.objects.filter(account__customer_id=self.customer_id)
+
+        transaction = transactions[0]
+
+        self.assertEquals(transaction.account.customer_id, self.customer_id)
+        self.assertEquals(transaction.amount, self.amount_to_add)
+        self.assertEquals(transaction.transaction_type, self.transaction_type)
