362544441a607af1585ce6891f1f2348a61b9ec2
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-01-23 10:05:54 +0530

    transaction type - green state

--- wallet/models/transaction.py
+++ wallet/models/transaction.py
@@ -4,11 +4,14 @@ from django.db import models
 class Transaction(models.Model):
     CUSTOMER_ID_LENGTH = 20
     TRANSACTION_ID_LENGTH = 20
+    CHOICES_MAX_LENGTH = 20
 
     customer_id = models.CharField(max_length=CUSTOMER_ID_LENGTH)
     transaction_id = models.CharField(max_length=TRANSACTION_ID_LENGTH,
                                       unique=True)
     amount = models.IntegerField()
+    type = models.CharField(max_length=CHOICES_MAX_LENGTH, choices=[
+        ('Credit', 'Credit'), ('Debit', 'Debit')])
 
     @classmethod
     def get_statement(cls, customer_id):
@@ -18,20 +21,23 @@ class Transaction(models.Model):
             transactions_list.append({
                 'customer_id': transaction.customer_id,
                 'transaction_id': transaction.transaction_id,
-                'amount': transaction.amount
+                'amount': transaction.amount,
+                'type': transaction.type
             })
         return transactions_list
 
     @classmethod
-    def add_transaction(cls, customer_id, amount):
+    def add_transaction(cls, customer_id, amount, type):
         transaction_id = cls.generate_transaction_id(cls.TRANSACTION_ID_LENGTH)
         cls.assign_transaction_id(customer_id=customer_id,
-                                  transaction_id=transaction_id, amount=amount)
+                                  transaction_id=transaction_id, amount=amount,
+                                  type=type)
 
     @classmethod
-    def assign_transaction_id(cls, customer_id, transaction_id, amount):
+    def assign_transaction_id(cls, customer_id, transaction_id, amount, type):
         cls.objects.create(customer_id=customer_id,
-                           transaction_id=transaction_id, amount=amount)
+                           transaction_id=transaction_id, amount=amount,
+                           type=type)
 
 
     @staticmethod
--- wallet/tests/test_get_statement.py
+++ wallet/tests/test_get_statement.py
@@ -24,14 +24,15 @@ class TestGetStatement(TestCase):
         from wallet.models import Transaction
         Transaction.assign_transaction_id(customer_id=self.sender_customer_id,
                                           transaction_id=transaction_id,
-                                          amount=50)
+                                          amount=50, type='Credit')
 
         sender_transactions_list = Transaction.get_statement(
             self.sender_customer_id)
 
         self.assertEquals(sender_transactions_list,
                           [{'customer_id': self.sender_customer_id,
-                            'transaction_id': transaction_id, 'amount': 50}])
+                            'transaction_id': transaction_id, 'amount': 50,
+                            'type': 'Credit'}])
 
     def testcase_unique_transaction_id(self):
         Account.add_balance(self.sender_customer_id, 50)
@@ -41,13 +42,13 @@ class TestGetStatement(TestCase):
         from wallet.models import Transaction
         Transaction.assign_transaction_id(customer_id=self.sender_customer_id,
                                           transaction_id=transaction_id,
-                                          amount=50)
+                                          amount=50, type='Credit')
 
         Account.add_balance(self.sender_customer_id, 20)
         with self.assertRaises(Exception):
             Transaction.assign_transaction_id(
                 customer_id=self.sender_customer_id,
-                transaction_id=transaction_id, amount=20)
+                transaction_id=transaction_id, amount=20, type='Credit')
 
     def testcase_add_debit_transaction(self):
         Account.add_balance(self.sender_customer_id, 50)
@@ -58,13 +59,13 @@ class TestGetStatement(TestCase):
         from wallet.models import Transaction
         Transaction.assign_transaction_id(customer_id=self.sender_customer_id,
                                           transaction_id=transaction_id_1,
-                                          amount=50)
+                                          amount=50, type='Credit')
 
         Account.transfer_balance(self.sender_customer_id,
                                  self.receiver_customer_id, amount=10)
         Transaction.assign_transaction_id(customer_id=self.sender_customer_id,
                                           transaction_id=transaction_id_2,
-                                          amount=10)
+                                          amount=10, type='Debit')
 
         sender_transactions_list = Transaction.get_statement(
             self.sender_customer_id)
