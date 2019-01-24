66b5a920b8e51758cbce7a1c507ffb2db787b08a
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-01-24 12:55:52 +0530

    Add balance should throw error for zero amount - green state

--- wallet/models/account.py
+++ wallet/models/account.py
@@ -24,7 +24,7 @@ class Account(models.Model):
 
     @classmethod
     def add_balance(cls, customer_id, amount):
-        if cls._is_negative(amount):
+        if cls._is_zero_or_negative(amount):
             raise Exception
 
         if cls._is_non_int_type(amount):
