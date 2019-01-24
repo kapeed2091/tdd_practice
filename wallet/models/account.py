b98b7ce4266f1e408846bedc69c6c03d64938847
Phaneeswar <phaneeswar@Phaneeswars-MacBook-Pro.local>
2019-01-24 11:44:53 +0530

    Refactoring: G10-Vertical Seperation, High-Low level functions seperation

--- wallet/models/account.py
+++ wallet/models/account.py
@@ -21,20 +21,6 @@ class Account(models.Model):
             account_id=account_id, customer_id=customer_id)
 
     @classmethod
-    def _is_customer_account_exists(cls, customer_id):
-        return cls.objects.filter(customer_id=customer_id).exists()
-
-    @classmethod
-    def _create_account(cls, account_id, customer_id):
-        return cls.objects.create(
-            account_id=account_id, customer_id=customer_id)
-
-    @staticmethod
-    def _generate_account_id(length):
-        import uuid
-        return str(uuid.uuid4())[0:length]
-
-    @classmethod
     def get_balance(cls, customer_id):
         account = cls.objects.get(customer_id=customer_id)
         return account.balance
@@ -45,37 +31,6 @@ class Account(models.Model):
         self._validate_amount(amount)
         self._add_balance(amount)
         Transaction.create_transaction(account=self, amount=amount)
-
-    def _add_balance(self, amount):
-        self.balance += amount
-        self.save()
-
-    def _remove_balance_if_amount_is_valid(self, amount):
-        from wallet.models import Transaction
-
-        self._validate_amount(amount)
-        self._validate_insufficient_balance(amount)
-        self._remove_balance(amount)
-        Transaction.create_transaction(account=self, amount=-amount)
-
-    def _remove_balance(self, amount):
-        self.balance -= amount
-        self.save()
-
-    @classmethod
-    def _validate_amount(cls, amount):
-        if cls._is_invalid_amount(amount):
-            raise Exception('Invalid amount')
-
-    def _validate_insufficient_balance(self, amount):
-        if amount > self.balance:
-            raise Exception('Insufficient balance')
-
-    @classmethod
-    def _is_invalid_amount(cls, amount):
-        if amount <= cls.MINIMUM_TRANSACTION_AMOUNT_SHOULD_BE_GREATER_THAN:
-            return True
-        return False
 
     @classmethod
     def get_account(cls, customer_id):
@@ -95,3 +50,48 @@ class Account(models.Model):
 
         sender_account._remove_balance_if_amount_is_valid(amount=amount)
         receiver_account.add_balance_if_amount_is_valid(amount=amount)
+
+    @classmethod
+    def _is_customer_account_exists(cls, customer_id):
+        return cls.objects.filter(customer_id=customer_id).exists()
+
+    @classmethod
+    def _create_account(cls, account_id, customer_id):
+        return cls.objects.create(
+            account_id=account_id, customer_id=customer_id)
+
+    @staticmethod
+    def _generate_account_id(length):
+        import uuid
+        return str(uuid.uuid4())[0:length]
+
+    def _add_balance(self, amount):
+        self.balance += amount
+        self.save()
+
+    def _remove_balance_if_amount_is_valid(self, amount):
+        from wallet.models import Transaction
+
+        self._validate_amount(amount)
+        self._validate_insufficient_balance(amount)
+        self._remove_balance(amount)
+        Transaction.create_transaction(account=self, amount=-amount)
+
+    @classmethod
+    def _validate_amount(cls, amount):
+        if cls._is_invalid_amount(amount):
+            raise Exception('Invalid amount')
+
+    @classmethod
+    def _is_invalid_amount(cls, amount):
+        if amount <= cls.MINIMUM_TRANSACTION_AMOUNT_SHOULD_BE_GREATER_THAN:
+            return True
+        return False
+
+    def _validate_insufficient_balance(self, amount):
+        if amount > self.balance:
+            raise Exception('Insufficient balance')
+
+    def _remove_balance(self, amount):
+        self.balance -= amount
+        self.save()
