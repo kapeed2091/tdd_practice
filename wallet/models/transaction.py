33991beee58d45d387e9525c60759b9e80e0b1d3
Deepak Kumar Malladi <kapeed2091@gmail.com>
2019-01-24 11:46:37 +0530

    N4 - Unambiguous Names

--- wallet/models/transaction.py
+++ wallet/models/transaction.py
@@ -15,10 +15,10 @@ class Transaction(models.Model):
 
     @classmethod
     def get_statement(cls, customer_id):
-        filtered_transactions = list(cls.objects.filter(
+        customer_transactions = list(cls.objects.filter(
             customer_id=customer_id))
         transactions_list = list()
-        for transaction in filtered_transactions:
+        for transaction in customer_transactions:
             transactions_list.append({
                 'customer_id': transaction.customer_id,
                 'transaction_id': transaction.transaction_id,
