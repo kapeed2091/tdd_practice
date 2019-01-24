a441e7341a38939d5595c90096f655159e4198ad
Ratan Rithwik <rithwik@shirley.local>
2019-01-24 20:28:15 +0530

    Green State: refactoring create_multiple_accounts_for_different_users test case

--- wallet/tests/test_create_account.py
+++ wallet/tests/test_create_account.py
@@ -17,15 +17,17 @@ class TestCreateAccount(TestCase):
     def testcase_create_multiple_accounts_for_different_users(self):
         customer_id_1 = 'customer1'
         customer_id_2 = 'customer2'
-        
+
         from wallet.models import Account
-        
-        account_details_1 = Account.create_account(customer_id_1)
-        account_details_2 = Account.create_account(customer_id_2)
-        
-        self.assertEquals(account_details_1['customer_id'], customer_id_1, 'Issue with customer_id')
-        self.assertEquals(account_details_2['customer_id'], customer_id_2, 'Issue with customer_id')
-        self.assertNotEqual(account_details_1['account_id'], account_details_2['account_id'])
+
+        Account.create_account(customer_id_1)
+        Account.create_account(customer_id_2)
+
+        account_objects = Account.objects.all()
+        customer_ids = [each.customer_id for each in account_objects]
+
+        self.assertEqual(account_objects.count(), 2)
+        self.assertItemsEqual(customer_ids, [customer_id_1, customer_id_2])
 
     def testcase_multiple_accounts_for_same_user(self):
         customer_id = 'customer1'
