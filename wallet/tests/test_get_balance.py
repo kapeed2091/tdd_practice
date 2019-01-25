10eedc63d9179c1aade35d0d37d80568659b6947
Ratan Rithwik <rithwik@shirley.local>
2019-01-25 09:30:49 +0530

    Refactoring

--- wallet/tests/test_get_balance.py
+++ wallet/tests/test_get_balance.py
@@ -8,10 +8,10 @@ class TestGetBalance(TestCase):
         from wallet.models import Account
         Account.create_account(self.customer_id)
         balance = Account.get_balance(self.customer_id)
-        self.assertEquals(balance, 0,
-                          'Incorrect balance for new account')
+        self.assertEquals(balance, Account.DEFAULT_BALANCE)
 
     def testcase_get_balance_without_customer_account(self):
         from wallet.models import Account
-        self.assertRaises(Account.DoesNotExist,
-                          lambda: Account.get_balance(self.customer_id))
+
+        with self.assertRaises(Account.DoesNotExist):
+            Account.get_balance(self.customer_id)
